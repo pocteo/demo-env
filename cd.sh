68c7d376234de62cfb57646b981061ffb737657d
Dridi Waild <hi@pocteo.io>
2019-12-17 00:27:33 +0100

    update cd

--- cd.sh
+++ cd.sh
@@ -5,4 +5,11 @@ git pull && \
 git checkout candidate && \
 
 # APPLY
-kubectl apply -f kubernetes.yaml
+kubectl apply -f kubernetes.yaml && \
+
+# SYNC
+export COMMIT_SHA=$(git rev-parse --short=7 HEAD)
+git fetch origin production && git checkout production && \
+git checkout $COMMIT_SHA kubernetes.yaml && \
+git commit -m "Manifest from commit $COMMIT_SHA" && \
+git push origin production
